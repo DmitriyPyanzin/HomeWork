a78dc0c6a07daa08e507c5e6dd98621b9e43fcdd
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-28 12:43:50 +0300

    Added_Task2_Topic1_Seminare1_For_C#

---
+++ Lesson_1._Introdutions_to_C#/main_tasks/Task2/C#/Desision/Program.cs
@@ -0,0 +1,9 @@
+﻿// Так как в задаче не указано число целое или вещественное, буду использовать тип double;
+Console.Write("Введите первое число: ");
+double num1 = Convert.ToInt64(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double num2 = Convert.ToInt64(Console.ReadLine());
+
+if(num1 < num2) Console.Write($"Число {num2} больше числа {num1}");
+else if(num1 > num2) Console.Write($"Число {num1} больше числа {num2}");
+else Console.Write($"Числа {num1} и {num2} равны");
