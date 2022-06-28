05c9fb3f27ff1b389870d46cdea23075d79c16f0
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-28 20:48:11 +0300

    Added_Task8_For_Topic1_Seminare1

---
+++ Lesson_1._Introdutions_to_C#/main_tasks/Task8/C#/Desision/Program.cs
@@ -0,0 +1,17 @@
+﻿//Даже если на ввод прийдет число вещественное, на выводе будут числа целочисленные;
+// и поэтому используем тип int;
+Console.Write("Привет! Введи число не меньше 2: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+if(number < 2) Console.WriteLine("Ты чё ввел!!! Сказано же не меньше 2.");
+else
+{
+    while(count <= number)
+    {
+        Console.Write($"{count}, ");
+        count++;
+    }
+    Console.WriteLine($"Вот все четные числа от 1 до {number} включительно!");
+}
+
+Console.WriteLine("Пока, пока!!!!");
