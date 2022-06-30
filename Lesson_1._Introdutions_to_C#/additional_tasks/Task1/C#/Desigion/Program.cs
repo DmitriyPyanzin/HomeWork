35c62a5202106688e323fb2c1cb461079580031c
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-30 06:44:24 +0300

    Added_Additional_Task1_For_Topic1_Seminare1

---
+++ Lesson_1._Introdutions_to_C#/additional_tasks/Task1/C#/Desigion/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число больше 0: ");
+double number = Convert.ToInt64(Console.ReadLine());
+while(number >= 1)
+{
+    Console.Write("1, ");
+    number--;
+}
+Console.WriteLine("ВСЁ!");
---
+++ Lesson_1._Introdutions_to_C#/additional_tasks/Task1/Python/Task1.py
@@ -0,0 +1,5 @@
+number = float(input("Введите число больше 0: "))
+while number >= 1:
+    print("1, ", end = " ")
+    number -= 1
+print("ВСЕ!")
