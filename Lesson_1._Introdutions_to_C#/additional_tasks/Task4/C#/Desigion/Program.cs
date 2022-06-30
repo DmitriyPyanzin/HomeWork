fa59115e4b6a586c6ade534910eb7b640da572ed
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-06-30 12:08:47 +0300

    Added_Additional_Task4_Topic1_Seminare1

---
+++ Lesson_1._Introdutions_to_C#/additional_tasks/Task4/C#/Desigion/Program.cs
@@ -0,0 +1,3 @@
+﻿Console.Write("Введите число секунд: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Это {second / 3600} час (-ов), {second % 3600 / 60} минут (-а), {second % 60} секунд (-а).");
