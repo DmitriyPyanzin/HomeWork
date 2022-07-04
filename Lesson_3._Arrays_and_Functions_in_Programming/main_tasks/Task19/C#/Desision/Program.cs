f8b484011d04ac6350b5874139ca4a9162a93e66
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-04 08:21:01 +0300

    Added_Task19_for_HomeWork_Topic1_Seminare3

---
+++ Lesson_3._Arrays_and_Functions_in_Programming/main_tasks/Task19/C#/Desision/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+FindPolindrom(number);
+
+void FindPolindrom (int num)
+{
+    int len = Convert.ToString(Math.Abs(num)).Length;
+    if(len != 5) Console.WriteLine("Нужно было ввести пятизначное число");
+    else
+    {
+        if(num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10)
+            Console.WriteLine($"Число {num} является полиндромом");
+        else Console.WriteLine($"Число {num} ни разу не полиндром");
+    }
+}
+
+void MySignature()
+{
+    Console.WriteLine();
+    Console.WriteLine("________________________");
+    Console.WriteLine("the program was implemented by Dmitry Pyanzin");
+    Console.WriteLine("________________________");
+}
+
+MySignature();
