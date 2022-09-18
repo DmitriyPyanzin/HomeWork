8c2b63413137f844ee3804e604253f63f18df32a
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-09-18 06:57:09 +0300

    Added_TextFile

--- 02._Getting_to_know_Python/Lesson_5._Accelerated_data_processing/Task_3.py
+++ 02._Getting_to_know_Python/Lesson_5._Accelerated_data_processing/Task_3.py
@@ -1,8 +1,12 @@
 # 4. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
 
-string = input() + ' '
-count = 1
+with open('g:/i_am_programmer/my_programs/HomeWork/02._Getting_to_know_Python/Lesson_5._Accelerated_data_processing/Task_3.txt', 'w', encoding = 'utf-8') as file:
+    file.write(input())
 
+with open ('g:/i_am_programmer/my_programs/HomeWork/02._Getting_to_know_Python/Lesson_5._Accelerated_data_processing/Task_3.txt', 'r', encoding = 'utf-8') as file:
+    string = file.read() + ' '
+
+count = 1
 for i in range(len(string)-1):
     if string[i] == string[i + 1]:
         count += 1
